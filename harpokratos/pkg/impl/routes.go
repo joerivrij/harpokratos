37a9b74cec15895a23d66588cef0ea42670da21e
joerivrij <joeri.vrijaldenhoven@cloudnation.nl>
2020-12-04 13:37:46 +0100

    some changes to the way secrets are gathered

--- harpokratos/pkg/impl/routes.go
+++ harpokratos/pkg/impl/routes.go
@@ -11,7 +11,7 @@ func InitRoutes(config config.HarpokratosConfig) *http.ServeMux {
 	serveMux := http.NewServeMux()
 
 	harpokratorHandler := HarpokratosHandler{Config: &config}
-	//pingpong
+
 	serveMux.HandleFunc("/ping", middleware.Adapt(harpokratorHandler.PingPong, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
 	serveMux.HandleFunc("/api/v1/health", middleware.Adapt(harpokratorHandler.VaultHealthCheck, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
 	serveMux.HandleFunc("/api/v1/secret", middleware.Adapt(harpokratorHandler.SecretHandler, middleware.ValidateRestMethod("POST"), middleware.LogRequestDetails()))
